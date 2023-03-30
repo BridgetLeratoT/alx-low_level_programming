6afae58e22f402913974242af1d026ee5079952d
BridgetLeratoT <leratotshabalala146@gmail.com>
2023-03-30 16:53:23 -0700

    print list

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,14 @@
+Task 0: print list 
+Prototype: size_t print_list(const list_t *h);
+
+Task 1: List lenth
+Prototype: size_t list_len(const list_t *h);
+
+Task 2: add node
+Prototype: list_t *add_node(list_t **head, const char *str);
+
+Task 3: add node at the end
+Prototype: list_t *add_node_end(list_t **head, const char *str);
+
+Task 4: free list
+Prototype: void free_list(list_t *head);
