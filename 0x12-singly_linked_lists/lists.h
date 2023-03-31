9c9543009cc72378dca281675502bef9992c659b
BridgetLeratoT <leratotshabalala146@gmail.com>
2023-03-30 17:07:34 -0700

    list_len

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,14 +1,48 @@
-Task 0: print list 
-Prototype: size_t print_list(const list_t *h);
+#ifndef LISTS_H
 
-Task 1: List lenth
-Prototype: size_t list_len(const list_t *h);
+#define LISTS_H
 
-Task 2: add node
-Prototype: list_t *add_node(list_t **head, const char *str);
 
-Task 3: add node at the end
-Prototype: list_t *add_node_end(list_t **head, const char *str);
+/**
+*
+*  * struct list_s - singly linked list
+*
+*   * @str: string - (malloc'ed string)
+*
+*    * @len: length of the string
+*
+*     * @next: points to the next node
+*
+*      *
+*
+*       * Description: singly linked list node structure
+*
+*        * for Holberton project
+*
+**/
 
-Task 4: free list
-Prototype: void free_list(list_t *head);
+typedef struct list_s
+
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+
+} list_t;
+
+
+size_t print_list(const list_t *h);
+
+size_t list_len(const list_t *h);
+
+list_t *add_node(list_t **head, const char *str);
+
+list_t *add_node_end(list_t **head, const char *str);
+
+void free_list(list_t *head);
+
+
+#endif
+
+
+
